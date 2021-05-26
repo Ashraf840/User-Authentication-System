af9f9063d55c17b7d57678ff2cf92dafcb0e576d
Ashraf840 <shift2unleashed2011@gmail.com>
2021-05-26 17:57:54 +0600

    Register user into the system & redirect to the home page

--- custom_user/u_account_app/forms.py
+++ custom_user/u_account_app/forms.py
@@ -7,3 +7,11 @@ class UserRegForm(UserCreationForm):
     class Meta():
         model = MyUser
         fields = ['email', 'company_name', 'phone', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['style'] = 'width: 550px; height: 35px;'
+        self.fields['company_name'].widget.attrs['style'] = 'width: 550px; height: 35px;'
+        self.fields['phone'].widget.attrs['style'] = 'width: 550px; height: 35px;'
+        self.fields['password1'].widget.attrs['style'] = 'width: 550px; height: 35px;'
+        self.fields['password2'].widget.attrs['style'] = 'width: 550px; height: 35px;'
