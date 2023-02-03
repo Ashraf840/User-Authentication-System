4b6b430fb7c95a9b363c05ef02cdaabbda8f8e51
ashraf840 <shift2unleashed2011@gmail.com>
2023-02-04 00:49:02 +0600

    Login API is setting up jwt-token at every successful login

--- authenticationApp/views.py
+++ authenticationApp/views.py
@@ -4,10 +4,13 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
 from rest_framework.exceptions import AuthenticationFailed
-
+import jwt, datetime
 
 
 class RegisterView(APIView):
+    """
+    User registration API
+    """
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -16,6 +19,9 @@ class RegisterView(APIView):
 
 
 class LoginView(APIView):
+    """
+    Login API to authenticate user using JWT authentication method.
+    """
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
@@ -28,6 +34,18 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed("Authentication failed")
 
-        return Response({
-            "message": "Successful"
-        })
+        # When successful, generate jwt
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),     # inteded to make token-expiry duration 60 mins from creation
+            'iat': datetime.datetime.utcnow(),
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)  # Enabling the HTTPOnly attribute prevents malicious scripts from stealing the user's session identity. Prohibit the frontend to access the token.
+        response.data = {
+            'jwt': token
+        }
+        return response
