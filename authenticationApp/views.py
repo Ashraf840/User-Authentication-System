0c4277e36e2c48ca34f88388999f4254115164fe
ashraf840 <shift2unleashed2011@gmail.com>
2023-02-03 19:06:40 +0600

    basic login implemented

--- authenticationApp/views.py
+++ authenticationApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+
 
 
 class RegisterView(APIView):
@@ -10,3 +13,21 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+
+        # check: incorrect email input
+        if user is None:
+            raise AuthenticationFailed('Authentication failed')
+        # check: incorrect password input
+        if not user.check_password(password):
+            raise AuthenticationFailed("Authentication failed")
+
+        return Response({
+            "message": "Successful"
+        })
