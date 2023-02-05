597283ddc12e939c570b36c27ef305095da9c69b
ashraf840 <shift2unleashed2011@gmail.com>
2023-02-05 17:37:16 +0600

    Basic auth implemented using default DRF login-logout urls; protected (basicAuth, sessionAuth) user-profile API

---
+++ backend/accounts/urls.py
@@ -0,0 +1,28 @@
+"""authSystem URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from accounts import views
+
+app_name = 'accountsApp'
+
+urlpatterns = [
+    path("profile/", views.ProfileView.as_view(), name="ProfileView"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+print("URLPatterns format-suffix-pattern:", urlpatterns)
