33d7edd45c7533708f24a3a304c7652afe747d4b
Ashraf840 <shift2unleashed2011@gmail.com>
2023-09-09 15:25:14 +0600

    Create customer User model; Custome UserAdmin

--- app/authenticationApp/admin.py
+++ app/authenticationApp/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .utils.adminUtils.utils import UserCreationForm, UserChangeForm
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["id", "email", "name", "tc", "is_admin"]
+    list_display_links = ["id", "email"]
+    list_filter = ["is_admin", "tc"]
+    fieldsets = [
+        ("User Credentials", {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["name", "tc"]}),
+        ("Permissions", {"fields": ["is_active", "is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "name", "tc", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email", "name"]
+    ordering = ["-id"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
--- app/authenticationApp/models.py
+++ app/authenticationApp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=255,
+        unique=True,
+    )
+    name = models.CharField(max_length=200)
+    tc = models.BooleanField(verbose_name="Terms & Conditions")
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(verbose_name='Created at', auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "tc"]
+
+    class Meta:
+        verbose_name_plural = 'User'
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
